e5f058eab78da77fc275c4b277d9201c788a71f4
Loc <buidinhloc231@gmail.com>
2024-06-27 22:54:39 +0700

    Update MatrixMul.cpp
    add desc

--- Data Structures/Others/MatrixMul.cpp
+++ Data Structures/Others/MatrixMul.cpp
@@ -1,29 +1,42 @@
 struct Matrix {
     int n, m;
     vector <vector <int> > x;
-
     vector<int> & operator[] (int i) { return x[i]; }
+    const vector<int> & operator[] (int i) const { return x[i]; }
 
-    Matrix(int r, int c) : x(r, vector <int> (c)), n(r), m(c) { }
+    /*    Matrix mat(n, m)    */
+    Matrix(int r, int c) : x(r, vector <int> (c)), n(r), m(c) {} 
 
-    Matrix(const vector<vector<int>> &x) : x(x), n(x.size()), m(x[0].size()) { }
+    /*    Matrix mat({...}, ..., {...})    */
+    Matrix(const vector<vector<int>> &x) : x(x), n(x.size()), m(x[0].size()) { } 
 
-    Matrix operator* (Matrix &b) {
+    
+    /*    a + b    */
+    Matrix operator+ (const Matrix &b) {
+        Matrix ans(m, n);
+        for(int i = 0; i < m; i++) 
+            for(int j = 0; j < n; j++)
+                (ans[i][j] = x[i][j] + b[i][j]) %= mod;
+        return ans;
+    }
+
+    /*    a * b    */
+    Matrix operator* (const Matrix &b) {
         Matrix& a = *this;
         assert(a.m == b.n);
         Matrix c(a.n, b.m);
         for (int i = 0; i < a.n; ++i)
             for (int j = 0; j < b.m; ++j)
-                for (int k = 0; k < a.m; ++k){
-                    c[i][j] += 1ll * a[i][k] * b[k][j] % mod;
-                    c[i][j] %= mod;
-                }
+                for (int k = 0; k < a.m; ++k)
+                    (c[i][j] += a[i][k] * b[k][j] % mod) %= mod;
         return c;
     }
 
-    Matrix pow(long long exp) {
+    /*    a ^ b    */
+    Matrix operator^ (int exp) {
         assert(n == m);
-        Matrix base = *this, ans = Matrix(n, n);
+        Matrix base = *this;
+        Matrix ans(n, n);
         for(int i=0; i<n; i++) ans[i][i] = 1;
         for (; exp > 0; exp >>= 1, base = base * base)
             if (exp & 1) ans = ans * base;

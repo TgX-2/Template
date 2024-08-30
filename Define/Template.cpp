b69fe59b41a24abe2dc5af96e71e39265f8e5065
Loc <buidinhloc231@gmail.com>
2024-08-30 23:23:07 +0700

    Create Template.cpp

---
+++ Define/Template.cpp
@@ -0,0 +1,103 @@
+/*-----------------------------
+        Author : TgX.2
+       11Ti - K28 - CHV
+-----------------------------*/
+
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b)       for (int i = (a), _b = (b); i <= _b; i += 1)
+#define FORD(i, a, b)      for (int i = (a), _b = (b); i >= _b; i -= 1)
+#define FORC(i, a, b, c)   for (int i = (a), _b = (b), _c = (c); i <= _b; i += _c)
+
+#define fi                 first
+#define se                 second
+#define pb                 push_back
+#define len(x)             (int)((x).size())
+#define all(x)             (x).begin(), (x).end()
+
+#define _                  << " " <<
+#define __                 << "\n"
+#define ___                << " "
+
+#define file               "temp"
+#define ______________TgX______________ main()
+#define int                long long
+#define intmax             1e9
+#define intmin            -1e9
+#define llongmax           1e18
+#define llongmin          -1e18
+#define memo(a, val)       memset((a), (val), sizeof((a)))
+
+using   namespace std;
+
+#define loc(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); cout << endl; }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cout << "[ " << *it << " = " << a << " ] ";
+    err(++it, args...);
+}
+
+template<typename T1, typename T2> ostream& operator << (ostream& out, const pair<T1, T2>& p) {
+    return out << "[ " << p.first << " , " << p.second << " ] ";
+}
+
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator << (ostream& out, const Con& con) {
+    out << "{ ";
+    for (auto beg = con.begin(), it = beg; it != con.end(); it++) {
+        out << (it == beg ? "" : ", ") << *it;
+    }
+    return out << " } ";
+}
+
+template <int D, typename T>
+struct Vec : public vector<Vec<D - 1, T>> {
+    template <typename... Args>
+    Vec(int n = 0, Args... args) : vector<Vec<D - 1, T>>(n, Vec<D - 1, T>(args...)) {}
+};
+
+template <typename T>
+struct Vec<1, T> : public vector<T> {
+    Vec(int n = 0, const T &val = T()) : vector<T>(n, val) {}
+};
+
+template<typename T1, typename T2> 
+bool mini(T1 &a, T2 b)
+    {if (a > b) a = b; else return 0; return 1;}
+
+template<typename T1, typename T2> 
+bool maxi(T1 &a, T2 b)
+    {if (a < b) a = b; else return 0; return 1;}
+/*-----------------------------*/
+
+
+void process() {
+    
+}
+
+
+
+/*-----------------------------*/
+______________TgX______________ {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);  
+    if (fopen(file".inp", "r")) {
+        freopen(file".inp", "r", stdin);
+        freopen(file".out", "w", stdout);
+    }
+    process();
+    cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC __ ;
+}
+
+
+/*==============================+
+|INPUT                          |
+--------------------------------|
+
+================================+
+|OUTPUT                         |
+--------------------------------|
+
+===============================*/

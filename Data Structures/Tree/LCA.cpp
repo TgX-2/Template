8615f368d3762054ed5707067331dcb1c09907fc
Loc <buidinhloc231@gmail.com>
2024-09-18 19:55:15 +0700

    Update LCA.cpp

--- Data Structures/Tree/LCA.cpp
+++ Data Structures/Tree/LCA.cpp
@@ -1,49 +1,43 @@
 struct LCA {
-    vector<int> euler, h, d;
-    vector<vector<int>> st;
-    int timer = 0;
+    vector<int> euler;
+    int h[maxn], d[maxn];
 
-    void dfs(int u, int cnt) {
-        d[u] = euler.size();
-        euler.push_back(u);
-        h[u] = cnt;
-        for (int v : g[u]) {
-            if (h[v] == -1) {
-                dfs(v, cnt + 1);
-                euler.push_back(u); 
-            }
+    int val[maxn], cnt = 0;
+    void dfs(int u, int pre = 0) {
+        d[u] = len(euler);
+        euler.pb(u);
+        h[u] = ++cnt;
+        for(const int &v: g[u]) if (v != pre) {
+            dfs(v, u);
+            euler.pb(u);
         }
     }
 
-    #define mmin(x, y) (h[x] < h[y]) ? x : y
+    #define mmin(x, y) ((h[x] < h[y]) ? x : y)
+    int st[20][maxn << 1];
+    void build() {
+        int m = len(euler);
+        FOR(i, 0, m - 1)
+            st[0][i] = euler[i];
 
-    void buildSparseTable() {
-        int m = euler.size();
-        st.assign(m, vector<int>(20));
-
-        for (int i = 0; i < m; i++) 
-            st[i][0] = euler[i];
-
-        for (int j = 1; (1 << j) <= m; j++) 
-            for (int i = 0; i + (1 << j) - 1 < m; i++) 
-                st[i][j] = mmin(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
-
+        for(int j = 1; (1 << j) <= m; j++)
+            for(int i = 1; i + (1 << j) - 1 < m; i++)
+                st[j][i] = mmin(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
     }
 
-    int query(int l, int r) {
-        int j = __lg(r - l + 1);
-        return mmin(st[l][j], st[r - (1 << j) + 1][j]);
+    int get(int l, int r) {
+        int k = __lg(r - l + 1);
+        return mmin(st[k][l], st[k][r - (1 << k) + 1]);
     }
 
     int lca(int u, int v) {
-        int left = d[u];
-        int right = d[v];
-        if (left > right) swap(left, right);
-        return query(left, right);
+        int l = d[u], r = d[v];
+        if (l > r) swap(l, r);
+        return get(l, r);
     }
 
-    LCA(int n) : h(n + 7, -1), d(n + 7) {
-        dfs(1, 0);
-        buildSparseTable();
+    LCA(int n) {
+        dfs(1);
+        build();
     }
 };

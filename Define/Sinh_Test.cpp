a85bc61a91694127a555b9c3b860552c75777265
Loc <buidinhloc231@gmail.com>
2024-11-27 22:53:43 +0700

    Update Sinh_Test.cpp

--- Define/Sinh_Test.cpp
+++ Define/Sinh_Test.cpp
@@ -1,72 +1,92 @@
+/*-----------------------------
+        Author : TgX.2
+       11Ti - K28 - CHV
+-----------------------------*/
+
 #include <bits/stdc++.h>
-using namespace std;
-const string name = "test";
-const string io   = "temp";
-const string trau = "trau";
-const int NTEST = 100;
+using   namespace std;
 
+#define FOR(i, a, b)       for (int i = (a), _b = (b); i <= _b; i += 1)
+#define FORD(i, a, b)      for (int i = (a), _b = (b); i >= _b; i -= 1)
 
-mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
-#define rand rd
+#define fi                 first
+#define se                 second
+#define pb                 push_back
+#define len(x)             (int)((x).size())
+#define all(x)             (x).begin(), (x).end()
 
-long long Rand(long long l, long long h) {
-    assert(l <= h);
-    return l + rd() * 1LL * rd() % (h - l + 1);
+#define _                  << " " <<
+#define __                 << "\n"
+#define ___                << " "
+
+#define ______________TgX______________ main()
+#define int                long long
+#define intmax             1e9
+#define intmin            -1e9
+#define llongmax           1e18
+#define llongmin          -1e18
+
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); cout << endl; }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cout << "[ " << *it << " = " << a << " ] ";
+    err(++it, args...);
 }
 
-bool bit01() {return Rand(1, 1e5)%2;}
-char az() {return (char)(Rand('a', 'z'));}
-char AZ() { return (char)(Rand('A', 'Z'));}
-char num() {return '0' + Rand(0, 9);}
+template<typename T1, typename T2> ostream& operator << (ostream& out, const pair<T1, T2>& p) {
+    return out << "[ " << p.first << " , " << p.second << " ] ";
+}
 
-
-string bignum(long long l)
-{
-    string s;
-    for(int i = 1; i <= l; i ++)
-    {
-        s += " ";
-        s[s.size() - 1] = num();
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator << (ostream& out, const Con& con) {
+    for (auto beg = con.begin(), it = beg; it != con.end(); it++) {
+        out << *it << " ";
     }
-    return s;
+    return out << "";
+}
+/*-----------------------------*/
+
+int getint(int l, int r) {
+    int res = 0;
+    for(int i = 0; i <= 3; i++) 
+        res = (res << 15) ^ (rand() & ((1 << 15) - 1));
+    return l + rand() % (r - l + 1);
 }
 
-string str(long long a) {
-    if(a == 0) return "0";
-    if(a < 0) return "-" + str(-a);
-    string ans = "";
-    for(int i = 1; i <= 4; i ++)
-    {
-        ans = "0" + ans;
-        ans[0] = (a % 10) + '0';
-        a /= 10;
-    }
-    return ans;
+vector<int> getarr(int n, int l, int r) {
+    vector<int> a(n);
+    for(int &x : a) 
+        x = getint(l, r);
+    return a;
 }
 
-void sinh() {
+vector<int> getpermutation(int n) {
+    vector<int> a(n);
+    iota(all(a), 1);
+    random_shuffle(all(a));
+    return a;
 }
 
 
-int main()
-{
+/*-----------------------------*/
+______________TgX______________ {
     srand(time(NULL));
-    freopen("result.txt", "w", stdout);
-    for (int iTest = 1; iTest <= NTEST; iTest++)
-    {
+    freopen("result.txt", "w", stderr);
+    for(int itest = 1; itest <= 1000; itest++) {
+        ofstream inp("temp.inp");
 
-        ofstream inp((io + ".inp").c_str());
-        /* Sinh test cho nay */
         inp.close();
 
-        system((name + ".exe").c_str());
-        system((trau + ".exe").c_str());
-
-        if (system(("fc " + io + ".out " + io + ".ans").c_str()) != 0) {
-            cout << "Test " << iTest << ": WRONG!\n";
+        system("temp.exe");
+        system("temp_trau.exe");
+        if (system("fc temp.ans temp.out") != 0) {
+            cerr << "Test " << itest << ": Wrong!"; 
             return 0;
-        } 
-        cout << "Test " << iTest << ": CORRECT!\n";
+        }
+        cerr << "Test " << itest << ": Right!" << "\n"; 
+        cerr << "---------------\n";
     }
-    return 0;
-}         
+}
+

736876243ff1e67a499aef8190a3a04bce1e96ea
Loc <buidinhloc231@gmail.com>
2025-11-12 16:12:59 +0700

    Update Template.cpp

--- Define/Template.cpp
+++ Define/Template.cpp
@@ -5,12 +5,13 @@
 
 #include <bits/stdc++.h>
 using   namespace std;
+bool STA_ALLOC;
 
-#ifdef TGX 
+#ifdef TGX
 #include "debug.h"
-#else 
+#else
 #define debug(...)
-#endif 
+#endif
 
 #define FOR(i, a, b)       for (int i = (a), _b = (b); i <= _b; i += 1)
 #define FORD(i, a, b)      for (int i = (a), _b = (b); i >= _b; i -= 1)
@@ -31,21 +32,25 @@ using   namespace std;
 
 
 
+
 void process() {
 
 }
 
 
 
+
 /*-----------------------------*/
+bool FIN_ALLOC;
 ______________TgX______________ {
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);  
-    if (fopen("temp.inp", "r")) {
-        freopen("temp.inp", "r", stdin);
-        freopen("temp.out", "w", stdout);
+    cin.tie(0); cout.tie(0);
+    if (fopen("repaint.inp", "r")) {
+        freopen("repaint.inp", "r", stdin);
+        freopen("repaint.out", "w", stdout);
     }
     process();
+    cerr << fixed << setprecision(2) << "Memo: " << (abs((&STA_ALLOC) - (&FIN_ALLOC)) / (1024.0 * 1024.0)) << " mb." __ ;
     cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s." __ ;
 }
 

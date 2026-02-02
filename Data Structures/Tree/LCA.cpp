76df5350a36cfbe63ecf81101d0d02de2a5d7857
Loc <buidinhloc231@gmail.com>
2026-02-02 22:25:24 +0700

    code truong thanh

--- Data Structures/Tree/LCA.cpp
+++ Data Structures/Tree/LCA.cpp
@@ -1,64 +1,48 @@
-class LCA {
-private:
-    vector<int> euler, dep, fir;
-    vector<vector<int>> st;
-    int n;
-    vector<int> h;
+vector<int> euler, dep;
+int h[maxn], fir[maxn];
+void dfs(int u, int pre, int d) {
+    fir[u] = len(euler);
+    euler.pb(u);
+    dep.pb(d);
+    h[u] = h[pre] + 1;
 
-    void dfs(int u, int pre, int d, const vector<vector<int>> &g) {
-        debug(u, pre);
-        fir[u] = len(euler);
+    for(const int &v : g[u]) if (v != pre) {
+        dfs(v, u, d + 1);
         euler.pb(u);
         dep.pb(d);
-        h[u] = h[pre] + 1;
-        
-        for(const int &v : g[u]) if (v != pre) {
-            dfs(v, u, d + 1, g);
-            euler.pb(u);
-            dep.pb(d);
+    }
+}
+
+int st[30][maxn << 1]; // nho nhan 2 maxn
+#define mmin(i, j) (dep[i] < dep[j] ? i : j)
+
+void buildst() {
+    int m = len(dep);
+    FOR(i, 0, m - 1) st[0][i] = i;
+
+    for(int j = 1; (1 << j) <= m; j++) {
+        for(int i = 0; i + (1 << j) + 1 <= m; i++) {
+            st[j][i] = mmin(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
         }
     }
+}
 
-    #define mmin(x, y) (dep[x] < dep[y] ? x : y)
+int getst(int l, int r) {
+    int k = __lg(r - l + 1);
+    return mmin(st[k][l], st[k][r - (1 << k) + 1]);
+}
 
-    void buildSparseTable() {
-        int m = len(dep);
-        int maxK = log2(m) + 7;
-        st.assign(m + 7, vector<int>(maxK));
-        FOR(i, 0, m - 1) st[i][0] = i;
+int getlca(int u, int v) {
+    int l = fir[u];
+    int r = fir[v];
+    if (l > r) swap(l, r);
+    return euler[getst(l, r)];
+}
 
-        for(int j = 1; (1 << j) <= m; j++) {
-            for(int i = 0; i + (1 << j) <= m; i++) {
-                st[i][j] = mmin(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
-            }
-        }
-    }
-
-    int getSparseTable(int l, int r) {
-        int k = r - l + 1;
-        int logK = log2(k);
-
-        return mmin(st[l][logK], st[r - (1 << logK) + 1][logK]);
-    }
-
-public:
-    LCA(const vector<vector<int>> &g, int root = 1) {
-        n = len(g);
-        fir.assign(n + 7, -1);
-        h.assign(n + 7, 0);
-        dfs(root, -1, 0, g);
-        buildSparseTable();
-    }
-
-    int getDis(int u, int v) {
-        int k = getLCA(u, v);
-        return h[u] + h[v] - 2 * h[k];
-    }
-
-    int getLCA(int u, int v) {
-        int l = fir[u];
-        int r = fir[v];
-        if (l > r) swap(l, r);
-        return euler[getSparseTable(l, r)];
-    }
-};
+/*
+Trong code main
+dfs(1, 0, 0);
+buildst();
+Lấy LCA của đỉnh u -> v, sử dụng:
+getlca(u, v);
+*/
